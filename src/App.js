6c5d1a961617c91b7650e1f9f16aaff4de66f196
Rory Combe <comberh@gmail.com>
2020-05-25 15:52:25 +0200

    Added react tailwind

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Icons, Tag } from 'react-tailwind-ui'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="p-8 bg-white">
+      <Tag icon={<Icons.Dollar />}>Money</Tag>
     </div>
   );
 }
